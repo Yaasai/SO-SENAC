ad080b72b08af704796ed6f087d38c0076947499
yaasai <santana.lucasg@gmail.com>
2020-10-23 18:29:55 -0300

    threads

---
+++ threads/main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+int contar_palavras(FILE *f) {
+    int palavras = 0;
+    char x[1024];
+    while (fscanf(f, " %1023s", x) == 1) {
+        palavras++;
+    }
+    return palavras;
+}
+
+void *parse_file(char *filename) {
+  FILE *fp;
+  if((fp = fopen(filename, "r")) != NULL) {
+    int numeroPalavras = contar_palavras(fp);
+    printf("%d\n", numeroPalavras);
+  }
+  fclose(fp);
+
+  return NULL;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+      printf("Pelo menos um arquivo é necessário.");
+      return 1;
+    }
+
+    pthread_t t[argc-1];
+
+    for (int i=1; i < argc; i++) { 
+        pthread_create(&t[i-1], NULL, (void *)parse_file, argv[i]); 
+    }
+    for (int i=1; i < argc; i++) { 
+        pthread_join(t[i-1], NULL); 
+    }
+
+    return 0;
+}
